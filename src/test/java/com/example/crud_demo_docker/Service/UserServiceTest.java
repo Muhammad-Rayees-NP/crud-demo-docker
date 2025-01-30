04e281e51f2e269a18001043f2288d16639f99e2
Muhammad Rayees N P <muhammadrayeesnp@gmail.com>
2025-01-30 16:41:54 +0400

    updated test cases

--- src/test/java/com/example/crud_demo_docker/Service/UserServiceTest.java
+++ src/test/java/com/example/crud_demo_docker/Service/UserServiceTest.java
@@ -5,10 +5,12 @@ import com.example.crud_demo_docker.Entity.User;
 import com.example.crud_demo_docker.Repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.ArgumentCaptor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -21,7 +23,7 @@ import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
     @Mock
