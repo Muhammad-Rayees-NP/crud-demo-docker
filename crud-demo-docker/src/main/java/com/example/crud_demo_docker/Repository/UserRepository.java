42466cc1fff42c2139c57fcc9bb2c38c30707421
Muhammad Rayees N P <muhammadrayeesnp@gmail.com>
2025-01-30 13:45:00 +0400

    changes corrected file structure

--- crud-demo-docker/src/main/java/com/example/crud_demo_docker/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.crud_demo_docker.Repository;
-
-import com.example.crud_demo_docker.Entity.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
